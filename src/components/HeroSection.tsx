1fd2681a31d66715a1539a71db09cc0e69be5c3b
NightW0lfff <philong170101@gmail.com>
2025-03-27 02:47:22 +1100

    Added Font Family Space Grotesk, Inter. Downgrade React to 18.* version to make it compatible with typewriter-effect. Added TypeWriter Effects.

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,40 +1,71 @@
+"use client";
+
 import React from "react";
 import Image from "next/image";
+import Typewriter from "typewriter-effect";
 
 interface HeroSectionProps {
-  title: string;
-  description: string;
-  image: string;
+	title: string;
+	description: string;
+	image: string;
 }
 
+const headlines = [
+	"Dream. Code. Coffee.",
+	"Your Daily Brew of Dev Thoughts",
+	"Tech News, One Sip at a Time",
+	"News, Opinions & Developer Brews",
+	"Stay Sharp. Stay Caffeinated",
+	"Fuel Your Dream. And Your Code",
+];
+
 const HeroSection: React.FC<HeroSectionProps> = ({
-  title,
-  description,
-  image,
+	title,
+	description,
+	image,
 }) => {
-  return (
-    <section className="flex flex-col items-center text-center">
-      <h1 className="text-6xl font-semibold py-4 w-2/4 leading-snug text-[var(--heading)] ">
-        {title}
-      </h1>
-      <p className="w-2/4 mx-auto mb-6">{description}</p>
-      <div className="flex justify-center gap-4 py-4 mb-30">
-        <button className="px-4 py-2 font-bold rounded cursor-pointer bg-[var(--accent)] text-white hover:bg-[var(--accent-hover)]">
-          Sign Up
-        </button>
-        <button className="px-4 py-2 font-semibold !bg-white !border !border-[var(--accent)] !text-[var(--accent)] rounded cursor-pointer hover:!bg-[var(--accent-light)] hover:!text-[var(--accent)] hover:!border-transparent">
-          Learn More
-        </button>
-      </div>
-      <Image
-        src={image}
-        alt="Hero Image"
-        width={1400}
-        height={800}
-        className="mx-auto bg-slate-400"
-      />
-    </section>
-  );
+	return (
+		<section className="flex flex-col items-center text-center">
+			<h1 className="text-6xl font-semibold w-2/4 leading-snug text-[var(--heading)] font-(family-name:--font-brand)">
+				Code & Coffee
+			</h1>
+
+			<h2 className="w-2/4 mb-6 ">
+				<span>
+					<Typewriter
+						options={{
+							strings: headlines,
+							cursor: "_",
+							autoStart: true,
+							loop: true,
+							delay: 70,
+							deleteSpeed: 30,
+							wrapperClassName: "text-2xl font-medium text-[var(--heading)] ",
+							cursorClassName: "text-2xl font-light animate-ping",
+						}}
+					/>
+				</span>
+			</h2>
+			<p className="w-2/4 mx-auto mb-6 text-lg italic font-medium">
+				" A community of developers who love to code and drink coffee. "
+			</p>
+			<div className="flex justify-center gap-4 py-4 mb-30">
+				<button className="px-4 py-2 font-bold rounded cursor-pointer bg-[var(--accent)] text-white hover:bg-[var(--accent-hover)]">
+					Sign Up
+				</button>
+				<button className="px-4 py-2 font-semibold !bg-white !border !border-[var(--accent)] !text-[var(--accent)] rounded cursor-pointer hover:!bg-[var(--accent-light)] hover:!text-[var(--accent)] hover:!border-transparent">
+					Learn More
+				</button>
+			</div>
+			<Image
+				src={image}
+				alt="Hero Image"
+				width={1400}
+				height={800}
+				className="mx-auto bg-slate-400"
+			/>
+		</section>
+	);
 };
 
 export default HeroSection;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,37 @@
 import React from "react";
 
 const Navbar = () => {
-  return (
-    <div className="flex items-center justify-between w-dvw py-10 px-16 bg-[var(--foreground)]">
-      <a href="#">
-        <h1 className="text-3xl font-bold text-[var(--logo)]">Code&Coffee</h1>
-      </a>
-      <nav>
-        <ul className="flex items-center justify-between gap-8 ">
-          <li className="text-[var(--text-light)]">
-            <a href="#">Home</a>
-          </li>
-          <li className="text-[var(--text-light)]">
-            <a href="#">News</a>
-          </li>
-          <li className="text-[var(--text-light)]">
-            <a href="#">Blogs</a>
-          </li>
-          <li className="text-[var(--text-light)]">
-            <a href="#">Categories</a>
-          </li>
-          <li className="text-[var(--text-light)]">
-            <a href="#">Contact</a>
-          </li>
-        </ul>
-      </nav>
-      <button className=" px-4 py-2 font-bold bg-[var(--accent)] text-white rounded cursor-pointer hover:bg-[var(--accent-hover)]">
-        Join
-      </button>
-    </div>
-  );
+	return (
+		<div className="flex items-center justify-between w-dvw py-10 px-16 bg-[var(--foreground)]">
+			<a href="#">
+				<h1 className="text-3xl text-[var(--logo)] font-(family-name:--font-brand) font-[700]">
+					&lt;Code&Coffee/&gt;
+				</h1>
+			</a>
+			<nav>
+				<ul className="flex items-center justify-between gap-8 ">
+					<li className="text-[var(--text-light)]">
+						<a href="#">Home</a>
+					</li>
+					<li className="text-[var(--text-light)]">
+						<a href="#">News</a>
+					</li>
+					<li className="text-[var(--text-light)]">
+						<a href="#">Blogs</a>
+					</li>
+					<li className="text-[var(--text-light)]">
+						<a href="#">Categories</a>
+					</li>
+					<li className="text-[var(--text-light)]">
+						<a href="#">Contact</a>
+					</li>
+				</ul>
+			</nav>
+			<button className=" px-4 py-2 font-bold bg-[var(--accent)] text-white rounded cursor-pointer hover:bg-[var(--accent-hover)]">
+				Join
+			</button>
+		</div>
+	);
 };
 
 export default Navbar;
