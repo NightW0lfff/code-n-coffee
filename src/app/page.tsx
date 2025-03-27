7f65afe702f5a438a8cc68a4552427317dbe3e4d
NightW0lfff <philong170101@gmail.com>
2025-03-27 21:21:48 +1100

    Fixed is not assignable to type

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,19 +7,15 @@ import Navbar from "@/components/Navbar";
 import NewsSection from "@/components/NewsSection";
 
 export default function Home() {
-  return (
-    <div className="container flex flex-col items-center gap-32 mx-auto mb-8">
-      <Navbar />
-      <HeroSection
-        title="Welcome to <Code&Coffee/>"
-        description="A community of developers and designers who love to code and drink coffee."
-        image="/images/hero.jpg"
-      />
-      <NewsSection />
-      <BlogSection />
-      <CategorySection />
-      <ContactSection />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="container flex flex-col items-center gap-32 mx-auto mb-8">
+			<Navbar />
+			<HeroSection image="/images/hero.jpg" />
+			<NewsSection />
+			<BlogSection />
+			<CategorySection />
+			<ContactSection />
+			<Footer />
+		</div>
+	);
 }
